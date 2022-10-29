850464fb1242e2d00f8eff4b3b44171eec0596bb
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-10-28 21:25:40 -0300

    feat: criação do model PaymentType

---
+++ database/migrations/2022_10_29_002320_create_payment_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paymenttype', function (Blueprint $table) {
+            $table->id('PaymentTypeID');
+            $table->string('Description', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_types');
+    }
+};
