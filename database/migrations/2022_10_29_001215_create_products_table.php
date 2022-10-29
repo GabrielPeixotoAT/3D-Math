a759c1abab1410771e9437244d42924562f24698
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-10-28 21:12:54 -0300

    feat: criação de models de Produto e Categoria

---
+++ database/migrations/2022_10_29_001215_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id('ProductID');
+            $table->string('Name', 100);
+            $table->decimal('Price',24);
+            $table->string('Description',900);
+
+            $table->unsignedBigInteger('CategoryIDFK');
+
+            $table->foreign('CategoryIDFK')->on('Category')->references('CategoryID');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
