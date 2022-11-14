b1f4b89192cd257e8ba1ff7a57c97c5a2a49b1e1
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-11-13 21:55:57 -0300

    feat: customização da view de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,12 @@
 <x-guest-layout>
+    <link rel="stylesheet" href="{{ asset( 'css/nicepage.css' ) }}" media="screen">
+    <link rel="stylesheet" href="{{ asset( 'css/3D-Math.css' ) }}" media="screen">
+    <script class="u-script" type="text/javascript" src="{{ asset( 'js/jquery.js' ) }}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{ asset( 'js/nicepage.js' ) }}" defer=""></script>
+    <meta name="generator" content="Nicepage 4.20.3, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link rel="stylesheet" href="{{ asset( 'css/bootstrap.min.css' ) }}">
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -23,7 +31,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+                <x-input-label for="password" :value="__('Senha')" />
 
                 <x-text-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -37,19 +45,19 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Lembrar de mim') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Esqueceu sua senha?') }}
                     </a>
                 @endif
 
                 <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('Entrar') }}
                 </x-primary-button>
             </div>
         </form>
