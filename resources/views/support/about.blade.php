0a3ec715961e189423a9b334788905b345fe8929
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-11-29 23:43:49 -0300

    fix: ajuste da escrita na secção sobre o projeto

--- resources/views/support/about.blade.php
+++ resources/views/support/about.blade.php
@@ -7,8 +7,8 @@
 <section class="u-align-center u-clearfix u-grey-5 u-section-8" id="sec-1802">
     <div class="u-clearfix u-sheet u-sheet-1">
         <h2 class="u-text u-text-default u-text-1">Sobre o Projeto</h2>
-        <p class="u-text u-text-2">Este é um projeto interdisciplinar (PI) no curso de Análise e
-            Desenvolvimento de Sistemas.
+        <p class="u-text u-text-2">Este é um projeto interdisciplinar (PI) do curso de Análise e
+            Desenvolvimento de Sistemas no IFBA campus Eunápolis.
         </p>
         <br>
         <div class="u-expanded-width u-list u-list-1">
